1585f55024ee8eb0c42c93df8ed6c1e89da4b8e8
Paul Hoffman <phoffman@proper.com>
2021-10-15 10:32:51 -0700

    More investigate

--- new_investigate_file.py
+++ new_investigate_file.py
@@ -6,7 +6,7 @@ import gzip, pickle, pprint, sys
 if not len(sys.argv) > 1:
 	exit("Arguments are a pickle.gz file, and optionally a record, to investigate")
 in_file = sys.argv[1]
-get_resp = 0
+get_resp = None
 if len(sys.argv) <= 3:
 	get_resp = int(sys.argv[2])
 with gzip.open(in_file, mode="rb") as f:
@@ -16,7 +16,7 @@ resp_count = 0
 print("There are {} responses".format(len(in_obj["r"])))
 for this_response in in_obj["r"]:
 	resp_count += 1
-	if get_resp == 0:
+	if get_resp == None:
 		print(resp_count)
 		pprint.pprint(this_response, compact=False)
 	elif get_resp == resp_count:
